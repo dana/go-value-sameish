d1ea662c9facd970f83deaf890d3ce413e810f78
Dana M. Diederich <diederich@gmail.com>
2015-11-15 17:11:52 -0800

    wip

--- valuesameish.go
+++ valuesameish.go
@@ -2,9 +2,45 @@ package valuesameish
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func Sameish(first interface{}, second interface{}) bool {
+	switch first.(type) {
+	case int:
+		switch second.(type) {
+		case int:
+			return first.(int) == second.(int)
+		case int8:
+			return first.(int8) == second.(int8)
+		case int16:
+			return first.(int16) == second.(int16)
+		case int32:
+			return first.(int32) == second.(int32)
+		case int64:
+			return first.(int64) == second.(int64)
+		case uint8:
+			return first.(uint8) == second.(uint8)
+		case uint16:
+			return first.(uint16) == second.(uint16)
+		case uint32:
+			return first.(uint32) == second.(uint32)
+		case uint64:
+			return first.(uint64) == second.(uint64)
+		case float32:
+			return first.(int) == int(second.(float32))
+		case float64:
+			return first.(int) == int(second.(float64))
+		case string:
+			i, _ := strconv.Atoi(second.(string))
+			return first.(int) == i
+		default:
+			return false
+		}
+
+	default:
+		return false
+	}
 	fmt.Println("Oh yeah")
-	return true
+	return false
 }
--- valuesameish_test.go
+++ valuesameish_test.go
@@ -6,7 +6,27 @@ import (
 	"testing"
 )
 
-func TestBasic(t *testing.T) {
+func TestBasicIntIntSame(t *testing.T) {
 	assert := assert.New(t)
 	assert.True(Sameish(1, 1))
+}
+func TestBasicIntIntNotSame(t *testing.T) {
+	assert := assert.New(t)
+	assert.False(Sameish(1, 2))
+}
+func TestBasicIntStringSame1(t *testing.T) {
+	assert := assert.New(t)
+	assert.True(Sameish(1, "1"))
+}
+func TestBasicIntFloatSame(t *testing.T) {
+	assert := assert.New(t)
+	assert.True(Sameish(1, 1.0))
+}
+
+func TestFirstMapFail(t *testing.T) {
+	assert := assert.New(t)
+	thing := map[string]interface{}{
+		"a": "b",
+	}
+	assert.False(Sameish(thing, 2))
 }
